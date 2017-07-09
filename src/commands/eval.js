0162be10f26d8b81ad1d1848edf3c39a426b2bdc
OGNova <jamooney2000@gmail.com>
2017-07-09 19:46:44 +0000

    Update Eval

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -4,16 +4,29 @@ function clean(text) {
     else
         return text;
 }
+
+const Discord = require('discord.js');
+const settings = require('../settings.json')
+
 exports.run = (client, message, args) => {
+
+
     if (message.author.id !== "166304313004523520") return;
     args = args.join(" ");
     try {
         var evaled = eval(args);
         if (typeof evaled !== 'string')
             evaled = require('util').inspect(evaled);
-        message.channel.send(`Input :inbox_tray: \`\`\`${args}\n\`\`\`\nOutput :outbox_tray: \`\`\`xl\n${clean(evaled)}\n\`\`\``);
+        //message.channel.send(`Input :inbox_tray: \`\`\`${args}\n\`\`\`\nOutput :outbox_tray: \`\`\`xl\n${clean(evaled)}\n\`\`\``);
+    const embed = new Discord.RichEmbed()
+        .setAuthor("Guardian Beta", "https://cdn.discordapp.com/avatars/329022281013657612/bcac7f90bdfbe3a29248456b8f52ebd3.png")    
+        .setColor(0x7289DA)
+        .setFooter(`NodeJS`, `https://cdn.discordapp.com/emojis/332982363212742678.png`)
+        .addField(`Input :inbox_tray:`, `\`\`\`\n${args}\n\`\`\``)
+        .addField(`Output :outbox_tray:`, `\`\`\`\n${clean(evaled)}\n\`\`\``)
+        message.channel.send({ embed })
     } catch (err) {
-        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        message.channel.send(`Input :inbox_tray: \`\`\`${args}\n\`\`\`\nOutput :outbox_tray: \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
 };
 
