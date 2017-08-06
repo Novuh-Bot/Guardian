33ba4219edf7985d350a3a68edd95b830bfdf29c
Unknown <jamooney2000@gmail.com>
2017-08-05 23:45:49 -0400

    Update todo.js

--- src/commands/todo.js
+++ src/commands/todo.js
@@ -1,24 +1,10 @@
-const fs = require('fs');
-const path = require('path');
-const todoPath = path.join(__dirname, '..', '/data/todo.json');
-let data = JSON.parse(fs.readFileSync(todoPath, 'utf8'));
-
-exports.run = (message, args) => {
-    let todo = args.slice(1).join(' ');
-    if(!args[0]) return;
-    if (!data) data ={
-        todo: [
-            {
-                "todo": `${todo}`
-            }
-        ]
-    };
-    fs.writeFileSync(todoPath, JSON.stringify(data));
-    fs.readFileSync(todoPath);
-    data.todo.push({
-        "todo": `${todo}`
-    });
-    message.channel.send(`I have added ${todo} to your todo list.`);
+exports.run = (client, message, args) => {
+    if (!args[0]) return;
+    if (args[0] === "todo") return message.reply("please specify what to add to the file");
+    args = args.join(" ");
+    message.reply("I've added your item to the file.");
+    const content = `**${message.author.username}#${message.author.discriminator}** (${message.author.id}) added a new item to their todo list:\n~~--------------------------------~~\n${args}\n~~--------------------------------~~\nOn the server: **${message.guild.name}**\nServer ID: **${message.guild.id}**`;
+    client.channels.get('​343600068378492931').send(`${content}`);
 };
 
 exports.conf = {
