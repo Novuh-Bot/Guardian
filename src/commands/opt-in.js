abac97466814cca19f3769e12810f1dff38f17e3
Unknown <jamooney2000@gmail.com>
2017-08-12 16:44:49 -0400

    Moar updates

--- src/commands/opt-in.js
+++ src/commands/opt-in.js
@@ -1,4 +1,3 @@
-
 exports.run =  (client, message) => {
     message.channel.send(`Due to the new Developer Terms of Service, I, OGNovuh#0014, need your, the users, permission to collect your data. The only time the bot, Guardian, collects your data is when you run the /userinfo command. The information is not stored anywhere. For more, visit https://github.com/Novuh-Bot/Guardian/blob/master/tos.md`)
 };
@@ -7,7 +6,7 @@ exports.conf = {
   enabled: true,
   guildOnly: true,
   aliases: [],
-  permLevel: 4
+  permLevel: 0
 };
 
 exports.help = {
