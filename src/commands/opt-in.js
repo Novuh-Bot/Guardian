0f7a133d701140b4b22490266e5d106fc3c45ec4
Unknown <jamooney2000@gmail.com>
2017-08-12 21:06:24 -0400

    Thing

--- src/commands/opt-in.js
+++ src/commands/opt-in.js
@@ -1,5 +1,20 @@
 exports.run =  (client, message) => {
-    message.channel.send(`Due to the new Developer Terms of Service, I, OGNovuh#0014, need your, the users, permission to collect your data. The only time the bot, Guardian, collects your data is when you run the /userinfo command. The information is not stored anywhere. For more, visit https://github.com/Novuh-Bot/Guardian/blob/master/tos.md`)
+    // message.channel.send(`Due to the new Developer Terms of Service, I, OGNovuh#0014, need your, the users, permission to collect your data. The only time the bot, Guardian, collects your data is when you run the /userinfo command. The information is not stored anywhere. For more, visit https://github.com/Novuh-Bot/Guardian/blob/master/tos.md`)
+      message.channel.send('What tag would you like to see?')
+      .then(() => {
+          message.channel.awaitMessages(response => response.content === 'yes', {
+              max: 1,
+              time: 30000,
+              errors: ['time'],
+          })
+          .then((collected) => {
+              if(collected.first().content == yes) {
+                  message.channel.send(`Thanks for opting into data collection!`)
+              } else {
+                  message.channel.send(`You have opted out of data collection.`)
+              }
+          })
+      })
 };
 
 exports.conf = {
