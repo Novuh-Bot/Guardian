d77c8bb04cc4f00614e74c2838328ff4e0b7f792
Unknown <jamooney2000@gmail.com>
2017-08-22 22:08:45 -0400

    5 days of work for an array and a typo. GG me.

--- src/commands/experimentaleval.js
+++ src/commands/experimentaleval.js
@@ -1,3 +1,5 @@
+const snekfetch = require('snekfetch');
+
 function clean(text) {
     if (typeof(text) === 'string')
         return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -6,14 +8,15 @@ function clean(text) {
 }
 
 exports.run = (client, msg, args) => {
+  args = args.join(" ");
   try {
     var evaled = eval(args);
     if (typeof evaled !== 'string')
             evaled = require('util').inspect(evaled);
     if (evaled.length > 1500) {
       const hasteURL = require('snekfetch')
-        .post('http://h.b1b1n-banned.me/documents')
-        msg.reply(`The output was over 1.5k characters, I have uploaded to hastebin. http://h.b1nb1n-banned.me/raw/${hasteURL.body.key}.`)
+        .post('http://h.b1nb1n-banned.me/documents')
+        msg.reply(`The output was over 1.5k characters, I have uploaded to hastebin. http://h.b1nb1n-banned.me/${hasteURL.body.key}.js.`)
     } else {
       msg.channel.send(`\`\`\`xl\n${clean(evaled)}\n\`\`\``);
     }
