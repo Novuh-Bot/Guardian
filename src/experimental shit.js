7f3fc7e40ac3ace573d8ae48161742d68b7ce30c
Unknown <jamooney2000@gmail.com>
2017-08-25 16:01:01 -0400

    Storage

--- src/experimental shit.js
+++ src/experimental shit.js
@@ -56,3 +56,57 @@ const embed = new Discord.RichEmbed()
   .addField('Reason', reason);
   message.channel.send(lang('BAN'));
 return client.channels.get(logchannel.id).send({embed});
+
+
+
+const { prefix } = require('../settings.json')
+
+async function embedSan(embed) {
+    embed.message ? delete embed.messsage : null
+    embed.footer ? delete embed.footer.embed : null
+    embed.provider ? delete embed.provider.emmbed : null
+    embed.thumbnail ? delete embed.thumbnail.embed : null
+    embed.image ? delete embed.image.embed : null
+    embed.author ? delete embed.author.embed : null
+    embed.fields ? embed.fields.forEach(f => {delete f.embed;}) : null
+    return embed;
+}
+
+
+exports.run = async (client, message, args) => {
+  const guild = message.guild;
+  const modlog = message.guild.channels.find('name', 'logs');
+  const caseNumber = args.shift();
+  const newReason = args.join(' ');
+
+  await modlog.fetchMessages({limit:100}).then((messages) => {
+      const caseLog = messages.filter(m => m.author.id === client.user.id &&
+        m.embeds[0] &&
+        m.embeds[0].type === 'rich' &&
+        m.embeds[0].footer &&
+        m.embeds[0].footer.text.startsWith('Case') &&
+        m.embeds[0].footer.text === `Case ${caseNumber}`
+      ).first();
+      modlog.fetchMessage(caseLog.id).then(logMsg => {
+          const embed = logMsg.embeds[0];
+          embedSan(embed);
+          embed.description == embed.description.replace(`Awaiting moderator's input. Use ${prefix}reason ${caseNumber} <reason>.`, newReason);
+          logMsg.edit({embed});
+      });
+  });
+};
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 2
+};
+  
+exports.help = {
+  name: 'reason',
+  description: 'Assigns a new reason to a specific case.',
+  usage: 'reason [case number] [reason]'
+};
+  
