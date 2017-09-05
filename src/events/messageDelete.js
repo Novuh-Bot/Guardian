7e14c186eea9ee03dbda61cca2e305d22d588e54
Unknown <jamooney2000@gmail.com>
2017-09-04 22:14:32 -0400

    Version 3.1.1
    Add Message Deletion event. Full changelog for Version 3 and later updates coming :soontm:

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,4 +1,5 @@
 module.exports = message => {
+    const client = message.client;
     const modlog = message.guild.channels.find('name', 'logs');
     if(!modlog) return console.log(`Message Deleted: ${message}`);
 };
