8f13e0c43c44b08ab9a01f8fac559c99dcad7ba0
Unknown <jamooney2000@gmail.com>
2017-09-12 12:02:24 -0400

    Rework unban and message delete event.

--- src/commands/unban.js
+++ src/commands/unban.js
@@ -1,13 +1,22 @@
-exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  client.unbanReason = reason;
-  client.unbanAuth = message.author;
-  let user = args[0];
-  let modlog = client.channels.find('name', 'logs');
+const {RichEmbed} = require('discord.js');
+const {caseNumber} = require('../util/caseNumber.js');
+const settings = require('../settings.json');
+
+exports.run = async (client, message, args) => {
+  const user = args[0]
+  const modlog = message.guild.channels.find('name', 'logs');
   if (!modlog) return message.reply('I cannot find a logs channel');
-  if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
-  if (!user) return message.reply('You must supply a User Resolvable, such as a user id.').catch(console.error);
+  const caseNum = await caseNumber(client, modlog);
+  if (!args[0]) return message.reply('You must provide a user resolvable to unban them.').catch(console.error);
+  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
+  
+  const embed = new RichEmbed()
+  .setColor(0x00AE86)
+  .setTimestamp()
+  .setDescription(`**Action:** Unban\n**Moderator:** ${message.author.username}#${message.author.discriminator}\n**Reason:** ${reason}`)
+  .setFooter(`Case ${caseNum}`);
   message.guild.unban(user);
+  return client.channels.get(modlog.id).send({embed});
 };
 
 exports.conf = {
--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,5 +1,5 @@
 module.exports = message => {
     const client = message.client;
     const modlog = message.guild.channels.find('name', 'logs');
-    if(!modlog) return console.log(`Message Deleted: ${message}`);
+    if(!modlog) console.log(`Message Deleted | Message Author: ${message.author.username}#${message.author.discriminator} | Message Content: ${message}`);
 };
