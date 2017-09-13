b2337831e4b114f05b5143151217a09e701a667e
Unknown <jamooney2000@gmail.com>
2017-09-12 22:13:40 -0400

    I FORGOT STUFF OOPS

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,5 +1,11 @@
+const { RichEmbed } = require('discord.js');
+
 module.exports = message => {
     const client = message.client;
     const modlog = message.guild.channels.find('name', 'logs');
+    const Deletion = new Discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setDescription(`**Action:** Message Deletion\n**Message Author:** ${message.author.username}#${message.author.discriminator}\n**Message Content:** ${message}`);
+    client.channels.get(modlog.id).send({Deletion});
     if(!modlog) console.log(`Message Deleted | Message Author: ${message.author.username}#${message.author.discriminator} | Message Content: ${message}`);
 };
---
+++ src/events/messageUpdate.js
@@ -0,0 +1,23 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = (message, oldMessage, newMessage) => {
+    if(oldMessage.author.bot) {
+        return false;
+    }
+
+    if (!oldMessage.guild) {
+        return false;
+    }
+
+    if (oldMessage.content == newMessage.content){
+        return false;
+    }
+
+    const client = message.client;
+    const modlog = message.guild.channels.find('name', 'logs');
+    const Difference = new Discord.RichEmbed()
+        .setColor(0x00AE86)
+        .setDescription(`**Action:** Message Edit\n**Message Author:** ${oldMessage.author}\n**Old Content:** ${oldMessage.content}\n**New Content:** ${newMessage.content}`);
+    client.channels.get(modlog.id).send({Difference});
+    if(!modlog) console.log(`Message EditED | Message Author: ${oldMessage.author} | Old Content: ${oldMessage.content} | New Content: ${newMessage.content}`);
+}
