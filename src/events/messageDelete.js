4449bf56ed18ce56d0e260141d97a01f1dcb6715
Unknown <jamooney2000@gmail.com>
2017-09-13 20:59:35 -0400

    A commit

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -3,7 +3,7 @@ const { RichEmbed } = require('discord.js');
 module.exports = message => {
      const client = message.client;
      const modlog = message.guild.channels.find('name', 'logs');
-     const Deletion = new Discord.RichEmbed()
+     const Deletion = new RichEmbed()
         .setColor(0x00AE86)
         .setDescription(`**Action:** Message Deletion\n**Message Author:** ${message.author.username}#${message.author.discriminator}\n**Message Content:** ${message}`);
      client.channels.get(modlog.id).send({ embed: Deletion });
