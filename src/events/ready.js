54346927712b9831199e4a6ca885093eab7e3925
Unknown <jamooney2000@gmail.com>
2017-09-15 13:13:07 -0400

    Version 3.1.1 | Log ready event

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,12 @@ const client = new Discord.Client();
 const help = `/help`
 module.exports = client => {
   const guildN = `${client.guilds.size}`
+  const ready = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Event:** Bot Ready\n**Ping:** client.ping\n**Server Count:** ${guildN}`);
   client.user.setPresence({ game: { name: `${help} | Protecting ${guildN} servers.`, type: 0 }});
   console.log(chalk.bgGreen.black(`Online and ready to serve ${guildN} servers.`));
+  client.channels.get('338067491349856259').send({ embed: ready })
   console.log(require('util').inspect(client.user.presence, {depth:2}))
 };
